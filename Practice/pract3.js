2aefe3d257fd3f5417f245a187d0f342dff3cf29
Yeuur <148925144+Alanunay@users.noreply.github.com>
2026-08-17 09:03:45 +0800

    Add files via upload

---
+++ Practice/pract3.js
@@ -0,0 +1,34 @@
+// let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for(let i = 0; i < 10; i++){
+//     console.log(`${number[i]}`);
+// }
+
+// let selector = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+//     for(let i = 0; i < selector.length; i++){
+//         if(selector[i] % 2 === 0){
+//             console.log(selector[i]);
+//         }
+//     }
+
+// let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for(let i = 9; i >= 0; i--){
+//     console.log(`${number[i]}`);
+// }
+//   console.log("Liftoff");
+
+
+// let numA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for(let i  = 0; i < 10; i++){
+//         console.log(` 7 x ${numA[i]} = ${7 * numA[i]}`);
+// }
+
+// const nums = [3, 67, 12, 90, 45, 8];
+// let largest = nums[0];
+// for(let i = 0; i < nums.length; i++){
+//     if(nums[i] > largest){
+//         largest = nums[i];
+//     }
+// }
+// console.log(largest);
+
+
