cbe99685720fac04b6026434c3a2e839c9039f85
Yeuur <148925144+Alanunay@users.noreply.github.com>
2026-08-17 09:04:21 +0800

    Add files via upload

---
+++ Html-js-css_Combined/dom.js
@@ -0,0 +1,58 @@
+// const element = document.getElementById("lister");
+// const firstChild = element.firstElementChild;
+// firstChild.style.backgroundColor = "brown";
+
+// const ulElements = document.querySelectorAll("ul");
+//     ulElements.forEach(ulElement => {
+//             const firstChild = ulElement.firstElementChild;
+//              firstChild.style.backgroundColor = "Yellow";
+//     });
+
+
+ // document.getElementById("click").style.fontSize = "45px";
+    // document.getElementById("").style.borderRadius = "35px";
+
+
+// function changeColor(){
+//     const ulElements = document.querySelectorAll("ul");
+//     ulElements.forEach(ulElement => {
+//             const firstChild = ulElement.firstElementChild;
+//              firstChild.style.backgroundColor = "Yellow";
+//     })
+// }
+
+// function changeColor(){
+//     const element = document.getElementById("lister");
+//     const secondChild = element.firstElementChild.nextElementSibling;
+//     secondChild.style.backgroundColor = "grey";
+
+
+
+// const element = document.getElementById("lister");
+// const nextSibling = element.nextElementSibling;
+// nextSibling.style.backgroundColor = "yellow";
+ 
+
+// const element = document.getElementById("Banana");
+// const prevSibling = element.previousElementSibling;
+// prevSibling.style.backgroundColor = "black";
+
+
+// const element = document.getElementById("Orange");
+// const parent = element.parentElement;
+// parent.style.backgroundColor = "red";
+
+// const element = document.getElementById("list");
+// const children = element.children;
+// children[2].style.backgroundColor = "red";
+
+// Array.from(children).forEach( child => {
+//     child.style.backgroundColor = "yellow";
+// }
+// )
+
+// const username = "Yeur";
+// const myUser = document.getElementById("msg");
+
+// myUser.textContent  →= username === "" ? 'Guest' : username;
+
---
+++ Html-js-css_Combined/new.js
@@ -0,0 +1,80 @@
+// function walkedDog(){
+//     setTimeout(() => {
+//         console.log("You walk the dog");
+//     }, 2500
+//     );
+// }
+// walkedDog();
+
+
+// setTimeout(() => {
+//     console.log("Task 1");
+// }, 15000);
+// console.log("Task 2");
+// console.log("Task 3");
+// console.log("Task 4");
+
+
+// function func1(callback){
+//     setTimeout(() => {
+//     console.log("Task 1");
+// callback()}, 15000);
+// }
+
+// function func2(){
+//     console.log("Task 2");
+//     console.log("Task 3");
+//     console.log("Task 4");
+// }
+// func1(func2);
+
+// function yeur(){
+//     document.getElementById("msg").style.color = "red";
+// }
+
+// hello(wait);
+// function hello(callback){
+//     console.log("Hello!");
+//     callback();
+// }
+// function goodbye(){
+//     console.log("Good bye!");
+// }
+// function wait(){
+//     console.log("wait!");
+// }
+
+sum(displayConsole, 20, 7, "+");
+function sum(callback, x, y, operator){
+    setTimeout(() =>{
+             let result = 0;
+            switch(operator){
+                case "+":
+                    result = x + y;
+                    break;
+
+                case "-":
+                    result = x - y;
+                    break;
+
+                case "*":
+                    result = x * y;
+                    break;
+
+                case "/":
+                    result = x / y;
+                    break;
+            }
+        callback(result);
+   }, 10000);
+}
+function displayConsole(result){  
+    if (result < 10) {
+        console.log(`The result: ${result} is low, need to improve`);
+    }
+    else if (result > 10) {
+        console.log(`The result: ${result} is high, outstanding performance`);
+    }
+}
+
+
